de74804cfeabefa5ee514921097438db4ccb1e09
Thomas Nguyen <tungvunguyennguyen@gmail.com>
2024-01-10 22:12:09 -0500

    Updated tag lines
    Notion AI is low-key great btw
    Azeebee is an underated font

--- docusarus/my-website/docusaurus.config.js
+++ docusarus/my-website/docusaurus.config.js
@@ -9,7 +9,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'comfySpace',
-  tagline: 'Intuitive Raspberry Pi interface',
+  tagline: 'Build Raspberry Pi interface with ease',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
--- docusarus/my-website/src/pages/index.js
+++ docusarus/my-website/src/pages/index.js
@@ -6,18 +6,19 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
-
+import RPiLogo from './/rpi_logo.png';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        
+    
         <Heading as="h1" className="hero__title">
-        Intuitive Raspberry Pi interface
+        Your <span className='underlined'>better</span>  <span ><a href='https://www.raspberrypi.com/'><img src={RPiLogo} width="80" className='RPIlogo'></img></a></span> interface
         </Heading>
         
-        <div className='get_started'style={{paddingBottom:'20px'}}>
+        <p className="hero__subtitle">made with <span className='underlined'>ease</span></p>
+        <div className='get_started'style={{paddingRight:'20px'}}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/First%20setup">
