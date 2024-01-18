d55b423f8b0cce324e75d4c4d86210ea02509cfe
Thomas Nguyen <tungvunguyennguyen@gmail.com>
2024-01-18 00:33:10 -0500

    Added waitlisting with Supabase

--- htmx/download/main.js
+++ htmx/download/main.js
@@ -1,4 +1,11 @@
+
+//import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2';
+// Create a single supabase client for interacting with your database
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+
+
 $(document).ready(function() {
+  
     $(".footer").load
     ("footer.html");
     $(".navbar").load
@@ -34,11 +41,47 @@ $(document).ready(function() {
         )
         });
 
+        $("#ios-waitlist-submit").click(async function(){
+          var ios_email = $("#ios-waitlist-email").val();
+          await AddIOSWaitlist(ios_email);
+        });
+
+
+
         $(".DownloadNotAvailable-linux").click(function(){
           $( "#waitlist-linux" ).attr(
             "open", true
           )
           });
 
+          $("#linux-waitlist-submit").click(async function(){
+            var linux_email = $("#linux-waitlist-email").val();
+            await AddLinuxWaitlist(linux_email);
+          });
+
+
 })
 
+async function AddIOSWaitlist(email){
+  
+  const supabase = createClient(
+    'https://pquytpdhjxhdgtutqyar.supabase.co', 
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBxdXl0cGRoanhoZGd0dXRxeWFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU1NTM1NzQsImV4cCI6MjAyMTEyOTU3NH0.TY_8c-4sYTLbobCt02IJdwBVa61hUcIgE4HsSl9vPKg'
+    );
+  const { error } = await supabase
+  .from('Waitlist')
+  .insert({Email: email.toString(), Platform: 'ios' });
+}
+
+async function AddLinuxWaitlist(email){
+  
+  const supabase = createClient(
+    'https://pquytpdhjxhdgtutqyar.supabase.co', 
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBxdXl0cGRoanhoZGd0dXRxeWFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU1NTM1NzQsImV4cCI6MjAyMTEyOTU3NH0.TY_8c-4sYTLbobCt02IJdwBVa61hUcIgE4HsSl9vPKg'
+    );
+  const { error } = await supabase
+  .from('Waitlist')
+  .insert({Email: email.toString(), Platform: 'linux' });
+  alert(error);
+  console.log(error);
+}
