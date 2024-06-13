2c1172a9970c9328b183194406074bbbb09745bf
Thomas Nguyen <tungvunguyennguyen@gmail.com>
2024-06-12 22:32:40 -0400

    Updated website - better theme & new logo

---
+++ hugo/v4/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    './layouts/**/*.html',
+    './content/**/*.md',
+    './content/**/*.html',
+    './themes/**/*.{html,js}',
+    './static/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
